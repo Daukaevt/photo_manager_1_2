7f9b24fdece915592b6558d707ae51dbb142ff36
Daukaevt <78514903+Daukaevt@users.noreply.github.com>
2025-02-06 21:28:00 +0300

    initial commit

---
+++ webMvc3_5-7/src/main/java/com/wixsite/mupbam1/WebMvc357Application.java
@@ -0,0 +1,13 @@
+package com.wixsite.mupbam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvc357Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvc357Application.class, args);
+	}
+
+}
---
+++ webMvc3_5-7/src/test/java/com/wixsite/mupbam1/WebMvc357ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wixsite.mupbam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvc357ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

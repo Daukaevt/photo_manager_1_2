36d3a0f557cb79397bec4347ae3127db4b4206c4
Daukaevt <mupbam1@gmail.com>
2025-03-11 15:52:13 +0300

    mysql DB remooved; postgresql added;

--- webMvc3_5-7/src/main/java/com/wixsite/mupbam1/WebMvc357Application.java
+++ webMvc3_5-7/src/main/java/com/wixsite/mupbam1/WebMvc357Application.java
@@ -1,5 +1,9 @@
 package com.wixsite.mupbam1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,15 @@ public class WebMvc357Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebMvc357Application.class, args);
+		String url = "jdbc:postgresql://localhost:5432/photo_album";
+		String user = "postgres";
+		String password = "bestuser";
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection successfull!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
